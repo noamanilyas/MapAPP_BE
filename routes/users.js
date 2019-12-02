ea4a604a71787ee306aa99c8da8fa118ab978ead
Noaman Ilyas <noamanilyas92@gmail.com>
2019-12-02 19:24:30 +0500

    Login

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let router = express.Router();
 let path = require('path');
+var moment = require("moment-timezone");
 
 /* GET users listing. */
 router.get('/getData', function(req, res, next) {
@@ -10,12 +11,22 @@ router.get('/getData', function(req, res, next) {
 
 router.post('/login', (req, res, next) => {
     let db = req.con;
-	db.query('SELECT Id FROM user WHERE UserName = ? AND Password = ?', [req.body.UserName, req.body.Password],function(err,rows){
+
+    let query = `SELECT Id FROM user WHERE Email = ? AND Password = ?`;
+    
+    let vals = [req.body.email, req.body.password]
+
+	db.query(query, vals,function(err,rows){
+        console.log("rowsss", rows)
+
 		if(err){
 			console.log("err", err);
         }
-        if(rows.length > 0){
-            res.send({'Status': 1, 'Msg': 'Login successfull.', 'Data': rows});
+        else if(rows.length > 0){
+            var resp = {
+                token: "afafdasdfsdfs"
+            }
+            res.send({'Status': 1, 'Msg': 'Login successfull.', 'Data': resp});
         } else {
             res.send({'Status': 0, 'Msg': 'Username or password incorrect.', 'Data': []});
             
@@ -30,19 +41,20 @@ router.post('/register', (req, res, next) => {
 		(FirstName, LastName, Email, Password) 
 		VALUES(?, ?, ?, ?)`;
 
-    console.log(req.body)
+    var createdDate = moment
+        .tz(Date.now(), 'America/New_York')
+        .format("YYYY-MM-DD HH:mm:ss");
 
- 	let vals = [req.body.firstName, req.body.lastName, req.body.email, req.body.password]
+ 	let vals = [req.body.firstName, req.body.lastName, req.body.email, req.body.password, ]
 
 	db.query(query, vals,function(err,rows){
 		if(err){
-			console.log("err", err);
             if(err.code == 'ER_DUP_ENTRY') {
                 res.send({'Status': 0, 'Msg': 'Registeration failed. Email already exists.', 'Data': []});
 
             }
         }
-        else if(rows.length > 0){
+        else if(rows.affectedRows == 1){
             res.send({'Status': 1, 'Msg': 'Registeration successfull.', 'Data': []});
         } else {
             res.send({'Status': 0, 'Msg': 'Registeration failed.', 'Data': []});
